7f7149aa6bf95f82795d0e037b3d3f74552980bd
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-08-15 12:23:27 +0600

    Pydantic - Custom Field Validation

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, field_validator
 
 
 class User(BaseModel):
     name: str
     email: EmailStr
     account_id: int
+
+    # Custom Field Validation
+    @field_validator('account_id')
+    def validate_account_id(cls, value):
+        if value <= 0:
+            raise ValueError(f"account_id must be positive: {value}")
+        return value
 
 
 user = User(name = "Rafi", email = "rafi@gmail.com", account_id = 1234)
