82c717d58f0adfc4672da2ac3c9a5903e24d269c
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-08-15 12:33:26 +0600

    Pydantic - JSON Serialization & Deserialization

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class User(BaseModel):
     email: EmailStr
     account_id: int
 
-    # Custom Field Validation
+    # Pydantic - Custom Field Validation
     @field_validator('account_id')
     def validate_account_id(cls, value):
         if value <= 0:
@@ -18,3 +18,15 @@ user = User(name = "Rafi", email = "rafi@gmail.com", account_id = 1234)
 print(user)
 
 
+# Pydantic - Json Serialization
+user_json_str = user.json()
+print(user_json_str)
+
+user_json_obj = user.dict()
+print(user_json_obj)
+
+
+# Pydantic - Json Deserialization
+json_str = '{"name": "user2", "email": "user2@gmail.com", "account_id": 1234}'
+user2 = User.parse_raw(json_str)
+print(user2)
